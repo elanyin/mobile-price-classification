4d347288977756b927b7723369b438edd1bd3c77
Yulong <yinyulong4@gmail.com>
2025-07-19 23:43:17 +0800

    Initial commit

---
+++ classification-server/src/main/java/com/elanyin/classificationserver/ClassificationServerApplication.java
@@ -0,0 +1,13 @@
+package com.elanyin.classificationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassificationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassificationServerApplication.class, args);
+    }
+
+}
---
+++ classification-server/src/test/java/com/elanyin/classificationserver/ClassificationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elanyin.classificationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassificationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
